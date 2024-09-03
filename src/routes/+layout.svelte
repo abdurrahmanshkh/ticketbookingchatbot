2e8f5ba781ba6bcce15bffd4ce01602e2018f1f5
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-09-03 16:33:06 +0530

    Updated design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,18 +4,18 @@
   	import { SearchOutline } from 'flowbite-svelte-icons';
 </script>
 
-<header class="border-2 p-2">
-	<Navbar>
+<header>
+	<Navbar class="bg-yellow-950 p-4">
 		<NavBrand href="/">
-			<span class="self-center whitespace-nowrap text-xl font-bold mr-2 text-primary-900">
+			<span class="self-center whitespace-nowrap text-xl font-bold mr-2 text-orange-100">
 				Chhatrapati Shivaji Maharaj Vastu Sangrahalaya
 			</span>
-			<span class="self-center whitespace-nowrap text-xl font-semibold text-primary-900">
+			<span class="self-center whitespace-nowrap text-xl font-semibold text-orange-100">
 				(CSMVS)
 			</span>
 		</NavBrand>
 		<NavBrand>
-			<span class="self-center whitespace-nowrap text-xl font-semibold text-blue-900">
+			<span class="self-center whitespace-nowrap text-xl font-semibold text-red-100">
 				Ticket Booking Chatbot
 			</span>
 		</NavBrand>
@@ -26,10 +26,10 @@
 	<slot/>
 </main>
 
-<footer class="border-2 p-2">
-	<Navbar>
+<footer>
+	<Navbar class="bg-yellow-950 p-4 text-orange-100">
 		<Footer rounded color="form" class="w-full flex items-center text-center justify-center">
-			<FooterCopyright href="/" by="Chhatrapati Shivaji Maharaj Vastu Sangrahalaya." year={2024} />
+			© Created as a sample chatbot website. All Rights Reserved.
 		</Footer>
 	</Navbar>
 </footer>
