4324dbf1af927bb8693b941bd3f7c5b77d73b6d1
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2025-06-20 23:58:19 +0530

    `Update svelte.config.js to include adapter regions configuration`

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			regions: ['bom1']
+		})
 	},
 	preprocess: vitePreprocess()
 };
+
 export default config;
